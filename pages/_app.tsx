ad9ee1aa17e3b7881c8e4828d60b6ffd2f18922f
CodeDetector <dhruvsharma983@gmail.com>
2024-08-06 02:32:53 +0530

    kinit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,37 +1,29 @@
 import { AppProps } from "next/app";
-import {SessionProvider} from 'next-auth/react'
+// import {SessionProvider} from 'next-auth/react'
 // import { Client as Styletron } from "styletron-engine-monolithic";
  
 import Layout from "../components/Layout";
 // import { LightTheme, BaseProvider } from "baseui";
 import '../styles/global.css'
-import {Inter} from  'next/font/google'
+// import {Inter} from  'next/font/google'
  
 // // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({
-  subsets: ['latin'],
-  weight: ['600'],
-  variable: '--font-inter',
-})
+// const inter = Inter({
+//   subsets: ['latin'],
+//   weight: ['600'],
+//   variable: '--font-inter',
+// })
 
 const App = ({ Component, pageProps }: AppProps) => {
 
   // const engine = new Styletron() ;
 
   return (
-    <SessionProvider session = {pageProps.session}>
-      {/* <StyletronProvider value={engine}>
-      <BaseProvider theme={LightTheme}> */}
-      <main className={`${inter.variable} font-sans` }>
+      
         <Layout   >
         <Component {...pageProps}  />
         </Layout>
-      </main>
       
-        
-      {/* </BaseProvider>
-      </StyletronProvider> */}
-    </SessionProvider>
     
   );
 };
