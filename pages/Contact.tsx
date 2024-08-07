c8de51b1f10a3ae26342e825ee6cd921cf7dab22
CodeDetector <dhruvsharma983@gmail.com>
2024-08-07 17:12:04 +0530

    commited

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react"
 import Image from "next/image"
-
+import styles from "../styles/contact.module.css"
 const Contact : React.FC = () =>{
 
     useEffect(()=>
@@ -10,14 +10,14 @@ const Contact : React.FC = () =>{
     ,[])
     return(
         <div style = {{width:"100vw" , height : "100vh"  }}>
-            <div style={{paddingTop:"5%" , }}>
+            <div className={styles.contact} style={{paddingTop:"5%" }}>
 
                 {/* <div>
                     <Image/>
                 </div> */}
                 <div style={{display:"flex", height:"100vh" , flexDirection:"column",fontFamily:"Helvetica"  ,width:"100%" ,  alignItems:"center"}}>
                     <h1 style={{marginBottom:"4%" , textTransform:"uppercase",textDecoration:"underline",textUnderlineOffset:"20%" ,fontSize: "xx-large"}}> contact </h1>
-                    <div style={{display:"flex",flexDirection:"column",fontFamily:"Helvetica",width:"100%" , paddingLeft:"5%"}}>
+                    <div style={{display:"flex",marginTop:"2rem",flexDirection:"column",fontFamily:"Helvetica",width:"100%" , paddingLeft:"5%"}}>
                     <h3 style={{textTransform:"uppercase"}}>Registered Address</h3>
                         <br/>5, Nirmala Ashram, Nirmala Chawani,Hardwar - 249401, Uttarakhand (INDIA)
                         <br/><br/><br/>
